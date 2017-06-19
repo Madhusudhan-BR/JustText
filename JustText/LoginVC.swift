de1c270bbe8548881f332243a3223648d5c59150
Madhusudhan-BR <madhusudhanbram@gmail.com>
2017-06-19 16:33:08 +0530

    login view setup done

--- JustText/LoginVC.swift
+++ JustText/LoginVC.swift
@@ -9,10 +9,151 @@
 import UIKit
 
 class LoginVC: UIViewController {
+    
+    let inputContainerView: UIView = {
+        //let view = UIView()
+        
+        let inputContainerView = UIView()
+        inputContainerView.backgroundColor = UIColor.white
+        inputContainerView.translatesAutoresizingMaskIntoConstraints = false
+        inputContainerView.layer.cornerRadius = 5
+        inputContainerView.layer.masksToBounds = true
+        
+        return inputContainerView
+    }()
+    
+    let resgisterButton: UIButton = {
+        
+        let button = UIButton(type: .system)
+        button.backgroundColor = UIColor.white
+        button.setTitle("Register", for: .normal)
+        button.layer.cornerRadius = 20
+        button.layer.masksToBounds = true
+        button.setTitleColor(UIColor(red: 244/255, green: 167/255, blue: 35/255, alpha: 1), for: .normal )
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 16)
+        button.translatesAutoresizingMaskIntoConstraints = false
+       
+        return button
+    }()
+    
+    let nameTextField : UITextField = {
+        
+        let tf = UITextField()
+        tf.placeholder = "Name"
+        tf.translatesAutoresizingMaskIntoConstraints = false
+        
+        return tf
+    }()
+    
+    let nameSeperatorView : UIView = {
+       let view = UIView()
+        view.backgroundColor = UIColor(red: 220/255, green: 220/255, blue: 220/255, alpha: 0.8)
+        view.translatesAutoresizingMaskIntoConstraints = false
+        
+        return view
+        
+    }()
+    
+    let emailTextField : UITextField = {
+        
+        let tf = UITextField()
+        tf.placeholder = "Email"
+        tf.translatesAutoresizingMaskIntoConstraints = false
+        
+        return tf
+    }()
+    
+    let emailSeperatorView : UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor(red: 220/255, green: 220/255, blue: 220/255, alpha: 0.8)
+        view.translatesAutoresizingMaskIntoConstraints = false
+        
+        return view
+        
+    }()
+
+    let passwordTextField : UITextField = {
+        
+        let tf = UITextField()
+        tf.isSecureTextEntry = true
+        tf.placeholder = "Password"
+        tf.translatesAutoresizingMaskIntoConstraints = false
+        
+        return tf
+    }()
+    
+    let profileImage: UIImageView = {
+        let img = UIImageView()
+        img.image = UIImage(named: "Group")
+        img.translatesAutoresizingMaskIntoConstraints = false
+        img.contentMode = .scaleAspectFill
+        return img
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = UIColor(red: 244/255, green: 167/255, blue: 35/255, alpha: 1)
+        
+        
+        view.addSubview(inputContainerView)
+        view.addSubview(resgisterButton)
+        view.addSubview(profileImage)
+        // x y width and height constraints 
+        
+        inputContainerView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        inputContainerView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        inputContainerView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 1, constant: -24).isActive = true
+        inputContainerView.heightAnchor.constraint(equalToConstant: 150).isActive = true
+        
+        inputContainerView.addSubview(nameTextField)
+        nameTextField.leftAnchor.constraint(equalTo: inputContainerView.leftAnchor , constant: 12 ).isActive = true
+        nameTextField.topAnchor.constraint(equalTo: inputContainerView.topAnchor).isActive = true
+        nameTextField.widthAnchor.constraint(equalTo: inputContainerView.widthAnchor).isActive = true
+        nameTextField.heightAnchor.constraint(equalTo: inputContainerView.heightAnchor, multiplier: 1/3).isActive = true
+        
+        inputContainerView.addSubview(nameSeperatorView)
+        nameSeperatorView.leftAnchor.constraint(equalTo: inputContainerView.leftAnchor).isActive = true
+        nameSeperatorView.topAnchor.constraint(equalTo: nameTextField.bottomAnchor).isActive = true
+        nameSeperatorView.widthAnchor.constraint(equalTo: inputContainerView.widthAnchor).isActive = true
+        nameSeperatorView.heightAnchor.constraint(equalToConstant: 1).isActive = true
+        
+        
+        
+        inputContainerView.addSubview(emailTextField)
+        emailTextField.leftAnchor.constraint(equalTo: inputContainerView.leftAnchor , constant: 12 ).isActive = true
+        emailTextField.topAnchor.constraint(equalTo: nameSeperatorView.topAnchor).isActive = true
+        emailTextField.widthAnchor.constraint(equalTo: inputContainerView.widthAnchor).isActive = true
+        emailTextField.heightAnchor.constraint(equalTo: inputContainerView.heightAnchor, multiplier: 1/3).isActive = true
+        
+        inputContainerView.addSubview(emailSeperatorView)
+        emailSeperatorView.leftAnchor.constraint(equalTo: inputContainerView.leftAnchor).isActive = true
+        emailSeperatorView.topAnchor.constraint(equalTo: emailTextField.bottomAnchor).isActive = true
+        emailSeperatorView.widthAnchor.constraint(equalTo: inputContainerView.widthAnchor).isActive = true
+        emailSeperatorView.heightAnchor.constraint(equalToConstant: 1).isActive = true
+
+        inputContainerView.addSubview(passwordTextField)
+        passwordTextField.leftAnchor.constraint(equalTo: inputContainerView.leftAnchor , constant: 12 ).isActive = true
+        passwordTextField.topAnchor.constraint(equalTo: emailSeperatorView.topAnchor).isActive = true
+        passwordTextField.widthAnchor.constraint(equalTo: inputContainerView.widthAnchor).isActive = true
+        passwordTextField.heightAnchor.constraint(equalTo: inputContainerView.heightAnchor, multiplier: 1/3).isActive = true
+
+        profileImage.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        profileImage.bottomAnchor.constraint(equalTo: inputContainerView.topAnchor, constant: -24).isActive = true
+        profileImage.widthAnchor.constraint(equalToConstant: 200).isActive = true
+        profileImage.heightAnchor.constraint(equalToConstant: 200).isActive = true
+        
+        setupRegisterButton()
+        
+    }
+    
+    func setupRegisterButton(){
+        
+        
+        resgisterButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        resgisterButton.topAnchor.constraint(equalTo: inputContainerView.bottomAnchor, constant: 12).isActive = true
+        resgisterButton.widthAnchor.constraint(equalTo: inputContainerView.widthAnchor).isActive = true
+        resgisterButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        
     }
     
     override var preferredStatusBarStyle: UIStatusBarStyle{
